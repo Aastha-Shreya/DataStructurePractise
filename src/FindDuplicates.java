5207d45ece4631e7aea3944ef7375d5ddd02eed6
Aastha-Shreya <aastha.shreya_cs19@gla.ac.in>
2021-08-18 23:48:32 +0530

    commit

--- src/FindDuplicates.java
+++ src/FindDuplicates.java
@@ -1,2 +1,18 @@
+import java.util.*;
+
+
 public class FindDuplicates {
+    //first we will use brut force technique to find the duplicates in a array.
+    public boolean duplicates(int[] arr){
+
+        int[] newArr = new int[];
+
+        for(int i =0; i<arr.length; i++){
+            newArr[arr[i]] += 1;
+            if(newArr[arr[i]]>1){
+                return true;
+            }
+        }
+        return false;
+    }
 }
